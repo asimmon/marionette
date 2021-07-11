2c034c785a9d46811b3d1f283185e4c6eb427503
Anthony Simmon <simmon.anthony@gmail.com>
2021-07-10 21:01:12 -0400

    Added moar tests

--- Askaiser.UITesting.Tests/TextElementRecognizerTests.cs
+++ Askaiser.UITesting.Tests/TextElementRecognizerTests.cs
@@ -17,6 +17,8 @@ namespace Askaiser.UITesting.Tests
         [InlineData(0, 0, 120, 30, "Google News", TextOptions.BlackAndWhite | TextOptions.Negative, 80, 18)]
         [InlineData(380, 170, 880, 336, "Headlines", TextOptions.None, 62, 17)]
         [InlineData(380, 170, 880, 336, "mperatures in southern califor", TextOptions.None, 172, 150)]
+        [InlineData(20, 810, 160, 860, "English (United States)", TextOptions.None, 67, 36)]
+        [InlineData(0, 0, 1920, 1080, "historic miami-dade courthouse closed due to ", TextOptions.None, 600, 665)]
         public async Task Recognize_WhenSingleMatch_Works(int x1, int y1, int x2, int y2, string searched, TextOptions options, int expectedX, int expectedY)
         {
             using var screenshot = await BitmapFromFile("./images/google-news.png");
@@ -26,6 +28,18 @@ namespace Askaiser.UITesting.Tests
             var result = await this._recognizer.Recognize(cropped, element);
 
             AssertResult(result, new Point(expectedX, expectedY));
+        }
+
+        [Fact]
+        public async Task Recognize_WhenMultipleMatches_Works()
+        {
+            using var screenshot = await BitmapFromFile("./images/google-news.png");
+            using var cropped = screenshot.Crop(new Rectangle(410, 300, 800, 550));
+            var element = new TextElement("California") { IgnoreCase = false };
+
+            var result = await this._recognizer.Recognize(cropped, element);
+
+            AssertResult(result, new Point(255, 18), new Point(306, 132));
         }
 
         public void Dispose()
