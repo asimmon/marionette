03d8642370038cece27433a58054764519410b6a
Anthony Simmon <simmon.anthony@gmail.com>
2021-07-15 20:26:53 -0400

    InternalsVisibleTo now in C# code instead of csproj

--- src/Askaiser.Marionette/TestContext.cs
+++ src/Askaiser.Marionette/TestContext.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Askaiser.Marionette.Commands;
+
+[assembly: InternalsVisibleTo("Askaiser.Marionette.Tests")]
 
 namespace Askaiser.Marionette
 {
